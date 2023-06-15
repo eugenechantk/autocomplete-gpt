6800555df9bd523a956d73a92640d768389b5818
swimmingcircle119 <lampmaa22@gmail.com>
2023-06-15 16:17:36 -0700

    removed openai stuff

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,27 +1,3 @@
-import { OpenAIApi, Configuration } from "openai";
-
-console.log("background script has loaded");
-console.log(process.env.OPENAI_API_KEY);
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-// Creating a new instance of the OpenAI class and passing in the OPENAI_KEY environment variable
-const openai = new OpenAIApi(configuration);
-const topic = 'NodeJs';
-const model = 'gpt-3.5-turbo';
-// Function to generate the prompt for the OpenAI API 
-// In the future, it will be moved to a helper class in the next code review
-const generatePrompt = (topic) => {
-    return `Write an blog post about "${topic}", it should in HTML format, include 5 unique points, using informative tone.`
-};
-// Use the generateText method to generate text from the OpenAI API and passing the generated prompt, the model and max token value
-const chat_completion = await openai.createChatCompletion({
-  model: model,
-  messages: [{ role: "user", content: generatePrompt(topic) }],
-});
-console.log(chat_completion);
-
-
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
   if (msg.action === "sendDOM") {
     console.log("Background script received dom content from content script")
